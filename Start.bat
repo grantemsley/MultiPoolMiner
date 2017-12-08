66f02128d61a49ab7701ae63da7f37efa2094022
Grant Emsley <gemsley@st-leonards.com>
2017-12-08 14:03:13 -0500

    Update start.bat to have currency setting

--- Start.bat
+++ Start.bat
@@ -8,13 +8,13 @@ setx GPU_SINGLE_ALLOC_PERCENT 100
 set bitcoinaddress=1BLXARB3GbKyEg8NTY56me5VXFsX2cixFB
 set miningpoolhubuser=grantemsley
 set workername=multipoolminer
-
+set currency=CAD,USD
 
 REM Get these from https://miningpoolhub.com/?page=account&action=edit
-set miningpoolhubapikey = 
+set miningpoolhubapikey=insertapikeyhere
 
 
-set "command=& .\multipoolminer.ps1 -wallet %bitcoinaddress% -username %miningpoolhubuser% -API_Key %miningpoolhubapikey% -workername %workername% -region US -currency btc -type amd,nvidia,cpu -poolname miningpoolhub,miningpoolhubcoins,zpool -donate 24 -watchdog"
+set "command=& .\multipoolminer.ps1 -wallet %bitcoinaddress% -username %miningpoolhubuser% -API_Key %miningpoolhubapikey% -workername %workername% -region US -currency %currency% -type amd,nvidia,cpu -poolname miningpoolhub,miningpoolhubcoins,zpool,ahashpool,hashrefinery -donate 24 -watchdog"
 powershell -noexit -executionpolicy bypass -windowstyle maximized -command "%command%"
 
 pause
