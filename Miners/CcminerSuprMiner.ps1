6afc437cd41cb9f671e8d4e528f61188b9e3624b
UselessGuru <uselessguru@gmail.com>
2018-03-30 00:11:41 +0200

    Add CcminerSuprMiner

---
+++ Miners/CcminerSuprMiner.ps1
@@ -0,0 +1,206 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [PSCustomObject]$Stats,
+    [PSCustomObject]$Config,
+    [PSCustomObject]$Devices
+)
+
+# Compatibility check with old MPM builds
+if (-not $Config.Miners) {return}
+
+# Hardcoded per miner version, do not allow user to change in config
+$MinerFileVersion = "2018032200" #Format: YYYYMMDD[TwoDigitCounter], higher value will trigger config file update
+$MinerBinaryInfo = "suprminer 1.2.7 (March 2018) optimized x16r algo without any dev fee"
+$Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
+$Path = ".\Bin\NVIDIA-SuprMiner\ccminer.exe"
+$Type = "NVIDIA"
+$API = "Ccminer"
+$Uri = "https://github.com/ocminer/suprminer/releases/download/1.2/suprminer-1.2.7z" # if new MinerFileVersion and new Uri MPM will download and update new binaries
+$UriManual = ""    
+$WebLink = "https://github.com/ocminer/suprminer" # See here for more information about the miner
+
+# Create default miner config, required for setup
+$DefaultMinerConfig = [PSCustomObject]@{
+    "MinerFileVersion" = "$MinerFileVersion"
+    "MinerBinaryInfo" = "$MinerBinaryInfo"
+    "Uri" = "$Uri"
+    "UriInfo" = "$UriManual"    
+    "Type" = "$Type"
+    "Path" = "$Path"
+    "Port" = 4068
+    #"IgnoreHWModel" = @("GPU Model Name", "Another GPU Model Name", e.g "GeforceGTX1070") # Available model names are in $Devices.$Type.Name_Norm, Strings here must match GPU model name reformatted with (Get-Culture).TextInfo.ToTitleCase(($_.Name)) -replace "[^A-Z0-9]"
+    "IgnoreHWModel" = @()
+    #"IgnoreDeviceID" = @(0, 1) # Available deviceIDs are in $Devices.$Type.DeviceIDs
+    "IgnoreDeviceID" = @()
+    "Commands" = [PSCustomObject]@{
+        "x16r"  = "" #X16R RavenCoin
+        "x16s"  = "" #X16S PigeonCoin
+    }
+    "CommonCommands" = ""
+    "DoNotMine" = [PSCustomObject]@{ 
+        # Syntax: "Algorithm" = "Poolname"
+        #"neoscrypt" = @("Zpool", "ZpoolCoins")
+    }
+}
+
+if (-not $Config.Miners.$Name.MinerFileVersion) {
+    # Read existing config file, do not use $Config because variables are expanded (e.g. $Wallet)
+    $NewConfig = Get-Content -Path 'config.txt' -ErrorAction Stop | ConvertFrom-Json -ErrorAction Stop
+    # Apply default
+    $NewConfig.Miners | Add-Member $Name $DefaultMinerConfig -Force -ErrorAction Stop
+    # Save config to file
+    $NewConfig | ConvertTo-Json -Depth 10 | Set-Content "config.txt" -Force -ErrorAction Stop
+    # Apply config, must re-read from file to expand variables
+    $Config = Get-ChildItemContent "Config.txt" -ErrorAction Stop | Select-Object -ExpandProperty Content
+}
+else {
+    if ($MinerFileVersion -gt $Config.Miners.$Name.MinerFileVersion) {
+        try {
+            # Read existing config file, do not use $Config because variables are expanded (e.g. $Wallet)
+            $NewConfig = Get-Content -Path 'Config.txt' | ConvertFrom-Json -InformationAction SilentlyContinue
+            
+            # Execute action, e.g force re-download of binary
+            # Should be the first action. If it fails no further update will take place, update will be retried on next loop
+            if ($Uri -and $Uri -ne $Config.Miners.$Name.Uri) {
+                if (Test-Path $Path) {Remove-Item $Path -Force -Confirm:$false -ErrorAction Stop} # Remove miner binary to force re-download
+                # Remove benchmark files, could by fine grained to remove bm files for some algos
+                # if (Test-Path ".\Stats\$($Name)_*_hashrate.txt") {Remove-Item ".\Stats\$($Name)_*_hashrate.txt" -Force -Confirm:$false -ErrorAction SilentlyContinue}
+            }
+
+            # Always update MinerFileVersion and download link, -Force to enforce setting
+            $NewConfig.Miners.$Name | Add-member MinerFileVersion "$MinerFileVersion" -Force
+            $NewConfig.Miners.$Name | Add-member Uri "$Uri" -Force
+
+            # Save config to file
+            $NewConfig | ConvertTo-Json -Depth 10 | Set-Content "Config.txt" -Force -ErrorAction Stop
+            # Apply config, must re-read from file to expand variables
+            $Config = Get-ChildItemContent "Config.txt" | Select-Object -ExpandProperty Content
+        }
+        catch {}
+    }
+}
+
+if ($Info) {
+    # Just return info about the miner for use in setup
+    # attributes without a curresponding settings entry are read-only by the GUI, to determine variable type use .GetType().FullName
+    return [PSCustomObject]@{
+        MinerFileVersion = $MinerFileVersion
+        MinerBinaryInfo  = $MinerBinaryInfo
+        Uri              = $Uri
+        UriManual        = $UriManual
+        Type             = $Type
+        Path             = $Path
+        Port             = $Port
+        WebLink          = $WebLink        
+        Settings         = @(
+            [PSCustomObject]@{
+                Name        = "Uri"
+                Controltype = "string"
+                Default     = $DefaultMinerConfig.Uri
+                Info        = "MPM automatically downloads the miner binaries from this link and unpacks them.`nFiles stored on Google Drive or Mega links cannot be downloaded automatically.`n"
+                Tooltip     = "If Uri is blank or is not a direct download link the miner binaries must be downloaded and unpacked manually (see README). "
+            }
+            [PSCustomObject]@{
+                Name        = "UriManual"
+                Controltype = "string"
+                Default     = $DefaultMinerConfig.UriManual
+                Info        = "Download link for manual miner binaries download.`nUnpack downloaded files to '$Path'."
+                Tooltip     = "See README for manual download and unpack instruction."
+            }
+            [PSCustomObject]@{
+                Name        = "IgnoreHWModel"
+                Controltype = "string[]"
+                Default     = $DefaultMinerConfig.IgnoreHWModel
+                Info        = "List of hardware models you do not want to mine with this miner, e.g. 'GeforceGTX1070'.`nLeave empty to mine with all available hardware. "
+                Tooltip     = "Detected $Type miner HW:`n$($Devices.$Type | ForEach-Object {"$($_.Name_Norm): DeviceIDs $($_.DeviceIDs -join ' ,')`n"})"
+            }
+            [PSCustomObject]@{
+                Name        = "IgnoreDeviceID"
+                Controltype = "int[]"
+                Default     = $DefaultMinerConfig.IgnoreDeviceID
+                Info        = "List of device IDs you do not want to mine with this miner, e.g. '0'.`nLeave empty to mine with all available hardware. "
+                Tooltip     = "Detected $Type miner HW:`n$($Devices.$Type | ForEach-Object {"$($_.Name_Norm): DeviceIDs $($_.DeviceIDs -join ' ,')`n"})"
+            }
+            [PSCustomObject]@{
+                Name        = "Commands"
+                Controltype = "PSCustomObject"
+                Default     = $DefaultMinerConfig.Commands
+                Info        = "Each line defines an algorithm that can be mined with this miner.`nOptional miner parameters can be added after the '=' sign. "
+                Tooltip     = "Note: Most extra parameters must be prefixed with a space"
+            }
+            [PSCustomObject]@{
+                Name        = "CommonCommands"
+                Controltype = "string"
+                Default     = $DefaultMinerConfig.CommonCommands
+                Info        = "Optional miner parameter that gets appended to the resulting miner command line (for all algorithms). "
+                Tooltip     = "Note: Most extra parameters must be prefixed with a space"
+            }
+            [PSCustomObject]@{
+                Name        = "DoNotMine"
+                Controltype = "PSCustomObject"
+                Default     = $DefaultMinerConfig.DoNotMine
+                Info        = "Optional filter parameter per algorithm and pool. MPM will not use the miner for this algorithm at the listed pool.`nSyntax: 'Algorithm_Norm = @(`"Poolname`", `"PoolnameCoins`")'. "
+                Tooltip     = "Not all pools are compatible with all miners and algorithms"
+            }
+        )
+    }
+}
+
+# Starting port for first miner
+$Port = $Config.Miners.$Name.Port
+
+# Get device list
+$Devices.$Type | ForEach-Object {
+
+    if ($DeviceTypeModel -and -not $Config.MinerInstancePerCardModel) {return} #after first loop $DeviceTypeModel is present; generate only one miner
+    $DeviceTypeModel = $_
+    $DeviceIDs = @() # array of all devices, ids will be in hex format
+    $DeviceIDs2gb = @() # array of all devices with less than 3MiB VRAM, ids will be in hex format
+
+    # Get DeviceIDs, filter out all disabled hw models and IDs
+    if ($Config.MinerInstancePerCardModel -and (Get-Command "Get-CommandPerDevice" -ErrorAction SilentlyContinue)) { # separate miner instance per hardware model
+        if ($Config.Miners.IgnoreHWModel -inotcontains $DeviceTypeModel.Name_Norm -and $Config.Miners.$Name.IgnoreHWModel -inotcontains $DeviceTypeModel.Name_Norm) {
+            $DeviceTypeModel.DeviceIDs | Where-Object {$Config.Miners.IgnoreDeviceID -notcontains $_ -and $Config.Miners.$Name.IgnoreDeviceID -notcontains $_} | ForEach-Object {
+                $DeviceIDs += [Convert]::ToString($_, 16) # convert id to hex
+            }
+        }
+    }
+    else { # one miner instance per hw type
+        $Devices.$Type | Where-Object {$Config.Miners.IgnoreHWModel -inotcontains $_.Name_Norm -and $Config.Miners.$Name.IgnoreHWModel -inotcontains $_.Name_Norm} | ForEach-Object {
+            $_.DeviceIDs | Where-Object {$Config.Miners.IgnoreDeviceID -notcontains $_ -and $Config.Miners.$Name.IgnoreDeviceID -notcontains $_} | ForEach-Object {
+                $DeviceIDs += [Convert]::ToString($_, 16) # convert id to hex
+            }
+        }
+    }
+
+    $Config.Miners.$Name.Commands | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp"  -and $Config.Miners.$Name.DoNotMine.$_ -inotcontains $Pools.(Get-Algorithm $_).Name -and $DeviceIDs} | ForEach-Object {
+
+        $Algorithm_Norm = Get-Algorithm $_
+
+        if ($Config.MinerInstancePerCardModel -and (Get-Command "Get-CommandPerDevice" -ErrorAction SilentlyContinue)) {
+            $Miner_Name = "$Name-$($DeviceTypeModel.Name_Norm)"
+            $Commands = Get-CommandPerDevice -Command $Config.Miners.$Name.Commands.$_ -Devices $DeviceIDs # additional command line options for algorithm
+        }
+        else {
+            $Miner_Name = $Name
+            $Commands = $Config.Miners.$Name.Commands.$_.Split(";") | Select -Index 0 # additional command line options for algorithm
+        }
+
+        [PSCustomObject]@{
+            Name             = $Miner_Name
+            Type             = $Type
+            Path             = $Path
+            Arguments        = ("-a $_ -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass)$Commands$($Config.Miners.$Name.CommonCommands) -b 127.0.0.1:$($Port) -d $($DeviceIDs -join ',')" -replace "\s+", " ").trim()
+            HashRates        = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
+            API              = $Api
+            Port             = $Port
+            URI              = $Uri
+            Fees             = @($null)
+            Index            = $DeviceIDs -join ';'
+            ShowMinerWindows = $Config.ShowMinerWindows
+        }
+    }
+    $Port++ # next higher port for next device
+}
