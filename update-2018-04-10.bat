fb595260e14e6be4e96ce38e60204e62604f7b72
Grant Emsley <grant@emsley.ca>
2018-04-29 14:22:37 -0400

    Remove update file

--- update-2018-04-10.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d %~dp0
-if exist "Stats\Fire*HashRate.txt" del "Stats\Fire*HashRate.txt"
-if exist "Stats\BMiner*HashRate.txt" del "Stats\BMiner*HashRate.txt"
-if exist "Stats\ClaymoreCpu*HashRate.txt" del "Stats\ClaymoreCpu*HashRate.txt"
