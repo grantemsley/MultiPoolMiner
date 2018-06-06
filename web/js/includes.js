74ccf7ad3a8aff4f36287440cd2765ab0a70cac7
Grant Emsley <grant@emsley.ca>
2018-06-06 11:48:20 -0400

    More changes

--- web/js/includes.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-	$('#includenavbar').load('/parts/navbar.html', function() { 
-		$("#"+$("body").data("navbaractive")).addClass("active");
-	});
-	$('#includeheader').load('/parts/header.html', function() {});
-});
