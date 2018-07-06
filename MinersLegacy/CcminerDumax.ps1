b82659c04980a26c8f3b2230e15c9b4e2f630c1f
Grant Emsley <grant@emsley.ca>
2018-07-06 16:02:51 -0400

    Update dumax

--- MinersLegacy/CcminerDumax.ps1
+++ MinersLegacy/CcminerDumax.ps1
@@ -1,10 +1,11 @@
 using module ..\Include.psm1
 
 $Path = ".\Bin\NVIDIA-Dumax\ccminer.exe"
-$HashSHA256 = "9F4F8B007847173037346C577483A181C392DFAF0B163E301E51FA336722D094"
-$Uri = "https://github.com/DumaxFr/ccminer/releases/download/dumax-0.9.1/ccminer-dumax-0.9.1-win64.zip"
+$HashSHA256 = "715EE772A023218044B14C5C9D7D41AA936F5FA5791EF10E4924A5DDAAF9B781"
+$Uri = "https://github.com/DumaxFr/ccminer/releases/download/dumax-0.9.3/ccminer-dumax-0.9.3-win64.zip"
 
 $Commands = [PSCustomObject]@{
+    "phi"           = "" 
     "phi2"          = "" #LUX
     "x16r"          = "" #X16r
     "x16s"         = "" #X16s
@@ -19,7 +20,9 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
 
     Switch ($Algorithm_Norm) {
         "PHI"   {$ExtendInterval = 3}
+        "PHI2"   {$ExtendInterval = 3}
         "X16R"  {$ExtendInterval = 10}
+        "X16S"  {$ExtendInterval = 10}
         default {$ExtendInterval = 0}
     }
 
@@ -27,7 +30,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
         Type           = "NVIDIA"
         Path           = $Path
         HashSHA256     = $HashSHA256
-        Arguments      = "-a $_ -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass)$($Commands.$_) --submit-stale"
+        Arguments      = "-R 1 -q -a $_ -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass)$($Commands.$_) --submit-stale"
         HashRates      = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Name)_$($Algorithm_Norm)_HashRate".Week}
         API            = "Ccminer"
         Port           = 4068
