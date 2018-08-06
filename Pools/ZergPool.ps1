a40306f7ae89f1ac03cb230cb76c96b8e33a5d3f
Grant Emsley <grant@emsley.ca>
2018-08-06 13:57:41 -0400

    Revert "Merge remote-tracking branch 'upstream/UselessGuru-ZergPool'"
    This reverts commit 8f9879d4522840a0bd140010cf80284cffa7cb9a, reversing
    changes made to 11476fa1364f338a81c4240bbabc5ac1b27bffe4.

--- Pools/ZergPool.ps1
+++ Pools/ZergPool.ps1
@@ -11,7 +11,6 @@ param(
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
 
 $ZergPool_Request = [PSCustomObject]@{}
-$ZergPoolCoins_Request = [PSCustomObject]@{}
 
 try {
     $ZergPool_Request = Invoke-RestMethod "http://api.zergpool.com:8080/api/status" -UseBasicParsing -TimeoutSec 10 -ErrorAction Stop
@@ -27,7 +26,7 @@ if (($ZergPool_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore
     return
 }
 
-$ZergPool_Regions = "europe"
+$ZergPool_Regions = "us", "europe"
 $ZergPool_Currencies = @("BTC", "DASH", "LTC") | Select-Object -Unique | Where-Object {Get-Variable $_ -ValueOnly -ErrorAction SilentlyContinue}
 $ZergPool_MiningCurrencies = ($ZergPoolCoins_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select-Object -ExpandProperty Name) | Foreach-Object {if ($ZergPoolCoins_Request.$_.Symbol) {$ZergPoolCoins_Request.$_.Symbol} else {$_}} | Select-Object -Unique # filter ...-algo
 
@@ -68,7 +67,7 @@ $ZergPool_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Se
                 StablePrice   = $Stat.Week
                 MarginOfError = $Stat.Week_Fluctuation
                 Protocol      = "stratum+tcp"
-                Host          = $ZergPool_Region.$ZergPool_Host
+                Host          = if ($ZergPool_Region -eq "us") {$ZergPool_Host}else {"$ZergPool_Region.$ZergPool_Host"}
                 Port          = $ZergPool_Port
                 User          = Get-Variable $_ -ValueOnly
                 Pass          = "$Worker,c=$_"
@@ -110,7 +109,7 @@ $ZergPool_MiningCurrencies | Where-Object {$ZergPoolCoins_Request.$_.hashrate -g
                     StablePrice   = $Stat.Week
                     MarginOfError = $Stat.Week_Fluctuation
                     Protocol      = "stratum+tcp"
-                    Host          = $ZergPool_Region.$ZergPool_Host
+                    Host          = if ($ZergPool_Region -eq "us") {$ZergPool_Host}else {"$ZergPool_Region.$ZergPool_Host"}
                     Port          = $ZergPool_Port
                     User          = Get-Variable $_ -ValueOnly
                     Pass          = "$Worker, c=$_, mc=$ZergPool_Currency"
@@ -130,7 +129,7 @@ $ZergPool_MiningCurrencies | Where-Object {$ZergPoolCoins_Request.$_.hashrate -g
                     StablePrice   = $Stat.Week
                     MarginOfError = $Stat.Week_Fluctuation
                     Protocol      = "stratum+tcp"
-                    Host          = $ZergPool_Region.$ZergPool_Host
+                    Host          = if ($ZergPool_Region -eq "us") {$ZergPool_Host}else {"$ZergPool_Region.$ZergPool_Host"}
                     Port          = $ZergPool_Port
                     User          = Get-Variable $_ -ValueOnly
                     Pass          = "$Worker,c=$_,mc=$ZergPool_Currency"
